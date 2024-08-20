ca615f46808ccf411a1c4cbbc0171384a62278ba
Alumno <Alumno@LAB1507-15>
2024-08-20 17:27:05 -0500

    Agregar archivos de proyecto.

---
+++ Demo01/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    internal class Persona
+    {
+
+        //public Persona(string Nombres, string Apellidos) 
+        //{
+        //    Nombres = Nombres;
+        //    Apellidos = Apellidos;
+        //}
+
+        public String Nombres { get; set; }
+        public String Apellidos { get; set; }
+
+        public String NombresCompletos { get; set; }
+
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Nombres + " " + Apellidos;
+        }
+
+    }
+
+}
---
+++ Demo01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int area = 0;
+            //int perimetro = 0;
+
+            //Rectangulo rectangulo = new Rectangulo();
+
+            //rectangulo.Base = 10;
+            //rectangulo.Altura = 20;
+
+            //area = rectangulo.CalcularArea();
+            //perimetro = rectangulo.CalcularPerimetro();
+
+            //Console.WriteLine("El area es: ");
+            //Console.WriteLine(area);
+            //Console.WriteLine("El perimetro es: ");
+            //Console.WriteLine(perimetro);
+
+
+            Persona persona = new Persona
+
+            {
+                Nombres = "Diego",
+                Apellidos = "Ferrer"
+            };
+
+            persona.AsignarNombresCompletos();
+
+            Console.WriteLine(persona.NombresCompletos);
+
+            Console.WriteLine("------------------------------------------------------------");
+
+            Console.WriteLine("Hello World"); //Escribe en pantalla
+            Console.Read(); //Le da un pare
+        }
+
+        static void Rectangulo() 
+        {
+            
+        }
+    }
+}
